68fe9f5d39e50f0c37535cdbb04fbbc6390eed2e
Phryxia <xahhaepica@gmail.com>
2019-02-21 15:27:13 +0900

    refactor
    Seperate common utility function from current source codes.

--- timetable.js
+++ timetable.js
@@ -5,35 +5,6 @@ let DOM_TB_CONFIG = document.getElementById('tb-config');
 let DOM_TB_TIMELINE = document.getElementById('tb-timeline');
 let DOM_TB_RESULT = document.getElementById('tb-result');
 
-function assert(cond) {
-	if(!cond) {
-		throw 'assertion failed';
-	}
-}
-
-/**
-	Give special ability to input_dom:
-
-	when cond(input_dom.value) is false, backup value
-	would be restored on given input dom.
-*/
-function invalid_input_protect(input_dom, cond) {
-	input_dom.onmousedown = function(evt) {
-		evt.toElement._last_val = evt.toElement.value;
-	};
-	input_dom.onchange = function(evt) {
-		if(!cond(evt.srcElement.value)) {
-			evt.srcElement.value = evt.srcElement._last_val;
-		}
-	};
-}
-
-/**
-*/
-function is_valid_float_string(s) {
-	return s.match(/^[0-9]+(\.[0-9]+)?$/) != null;
-};
-
 /**
 	Convert minute-specified integer into
 	hh:mm string.
