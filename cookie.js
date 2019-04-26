67a56d89588bb158c046a9d66a9203470a81a5ea
Phryxia <xahhaepica@gmail.com>
2019-04-26 13:41:47 +0900

    bug fix
    cookie 입출력이 매우 불안정하며, 구조 변경이 유연하지 못함.
    
    localforage.js 라이브러리를 이용하여, IndexedDB API를 사용, 쿠키를 대체하였음. 이제 쿠키가 날아가는 문제가 해결되었음.

--- cookie.js
+++ cookie.js
@@ -5,13 +5,73 @@
 	application and cookie.
 */
 class CookieManager {
+	/**
+		로컬에 저장된 설정을 혀재 세션의 presetController에 불러온다.
+	*/
 	load_snapshot(prsctr) {
 		assert(!!prsctr && prsctr instanceof PresetController);
+		this.prsctr = prsctr;
 		let success = true;
 		let obj = null;
 		// load JSON object
+		this.__load_cookie();
+	}
+
+	/**
+		현재 세션의 presetController의 설정을 로컬에 저장한다.
+	*/
+	save_snapshot(prsctr) {
+		assert(!!prsctr && prsctr instanceof PresetController);
+		this.prsctr = prsctr;
+		this.__save_cookie(JSON.stringify({
+			presets: prsctr.presets,
+			selected_index: prsctr.selected_index
+		}));
+	}
+
+	// is_local() {
+	// 	return window.location.origin.match(/^file:/) != null;
+	// }
+
+	/**
+		JSONString을 localforage를 사용하여 로컬에 저장한다.
+
+		(추후 object로 변경될 예정이다)
+	*/
+	__save_cookie(pure_json_str) {
+		localforage.setItem('json_string', pure_json_str).catch(function(err) {
+			console.log(err);
+		});
+	}
+
+	/**
+		localforage에서 저장된 정보를 불러온 뒤, 현재 세션의 preset
+		Controller에 적용한다.
+
+		만약 도중 에러가 발생할 경우, __reset_cookie()를 호출한다.
+	*/
+	__load_cookie() {
+		let self = this;
+		localforage.getItem('json_string').then(function(val) {
+			self.loaded_cookie = val;
+			self.__apply_cookie();
+		}).catch(function(err) {
+			// error recvoery
+			console.log(err);
+			self.__reset_cookie();
+		});
+	}
+
+	/**
+		__load_cookie()에서 호출된 localforage의 비동기 처리완료
+		콜백 함수다.
+
+		만약 도중 에러가 발생할 경우, __reset_cookie()를 호출한다.
+	*/
+	__apply_cookie() {
+		let obj = null;
 		try {
-			obj = JSON.parse(this.__load_cookie());
+			obj = JSON.parse(this.loaded_cookie);
 		}
 		catch(e) {
 			console.log(e);
@@ -23,60 +83,17 @@ class CookieManager {
 				obj.presets[i] = new Preset(obj.presets[i].name, new Config(obj.presets[i].config));
 			
 			// apply to PresetController
-			prsctr.override_presets(obj.presets, obj.selected_index);
+			this.prsctr.override_presets(obj.presets, obj.selected_index);
 		} else {
 			// just use default preset.
-			prsctr.set_current(0);
+			this.__reset_cookie();
 		}
 	}
 
-	save_snapshot(prsctr) {
-		assert(!!prsctr && prsctr instanceof PresetController);
-		this.__save_cookie(JSON.stringify({
-			presets: prsctr.presets,
-			selected_index: prsctr.selected_index
-		}));
-	}
-
-	is_local() {
-		return window.location.origin.match(/^file:/) != null;
-	}
-
-	__save_cookie(pure_json_str) {
-		// append expiration date
-		let str = '';
-		let expire_date = new Date();
-		expire_date.setTime(expire_date.getTime() + 3600 * 24 * 365);
-		str += 'content=' + pure_json_str + ';';
-		str += 'domain=krissvector.moe;';
-		str += 'expires=' + expire_date.toGMTString() + ';';
-
-		// escape
-		str = encodeURIComponent(str);
-
-		// set cookie
-		if(window.location.origin.match(/^file:/) != null)
-			window.localStorage.setItem('cookie', str);
-		else
-			document.cookie = str;
-	}
-
-	__load_cookie() {
-		// load cookie
-		let raw_str;
-		if(window.location.origin.match(/^file:/) != null)
-			raw_str = window.localStorage.getItem('cookie');
-		else
-			raw_str = document.cookie;
-
-		// unescape
-		raw_str = decodeURIComponent(raw_str);
-
-		// parse
-		let pairs = raw_str.split(';');
-		if(pairs[0].indexOf('=') == -1)
-			return "";
-		else
-			return pairs[0].substr(pairs[0].indexOf('=') + 1);
+	/**
+		기본 프리셋을 presetController에 로드시킨다.
+	*/
+	__reset_cookie() {
+		this.prsctr.set_current(0);
 	}
 }
