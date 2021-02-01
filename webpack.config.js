05790f178959f2ac1bcae84a19a8496dc14d6c42
Phryxia <xahhaepica@gmail.com>
2021-02-01 11:46:37 +0900

    webpack으로 번들링할 수 있게 만듦
    - npm 설정
    - webpack 설정
    - babel 설치

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/init.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
